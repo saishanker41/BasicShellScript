4abb9b32a0c6dd43bd627a94938c5cedb4fa7387
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-02-03 18:41:10 +0530

    Few Basic Shell Script programs done need to mv remaining programs from linux-content

---
+++ moveFiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+for files in `ls *.txt`;
+do
+    folderName=`echo $files | awk -F. '{ print $1}'`;
+    if [ -d $folderName ];
+    then
+        rm -R $folderName;
+    fi
+    
+    mkdir $folderName;
+    cp $files $folderName;
+done
